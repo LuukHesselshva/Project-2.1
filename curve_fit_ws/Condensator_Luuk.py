c8e8f3e8e667a547f9cbf74f9dd0d4984f9e18d0
LuukHesselshva <luuk.hessels@hva.nl>
2025-09-16 16:53:16 +0200

    Create Condensator_Luuk.py

---
+++ curve_fit_ws/Condensator_Luuk.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np 
+import matplotlib.pyplot as plt
+import scipy.optimize as opt
+import os
+
+os.chdir("C:\\Users\\Win11 Pro\\Documents\\Project-2.1\\curve_fit_ws")
+df = pd.read_csv('Condensator.csv')
