4dfcf94cd31c81c803fd0a2aa9c5b30a6351255c
LuukHesselshva <luuk.hessels@hva.nl>
2025-09-16 18:27:37 +0200

    Update Condensator_Luuk.py

--- curve_fit_ws/Condensator_Luuk.py
+++ curve_fit_ws/Condensator_Luuk.py
@@ -4,5 +4,24 @@ import matplotlib.pyplot as plt
 import scipy.optimize as opt
 import os
 
+def Spanning(t,V0,tau):
+    return V0*(1-np.exp(-t/tau))
+
 os.chdir("C:\\Users\\Win11 Pro\\Documents\\Project-2.1\\curve_fit_ws")
 df = pd.read_csv('Condensator.csv')
+
+t = np.array(df['tijd (ms)'])
+V = np.array(df['spanning (V)'])
+
+popt,pcov = opt.curve_fit(Spanning,t,V)
+tau,V0 = popt
+span = Spanning(t,V0,tau)
+
+plt.scatter(t,V,color='blue')
+plt.plot(t,span,color='red')
+plt.xlabel('tijd (s)')
+plt.ylabel('spanning (V)')
+plt.title('spanning over tijd')
+plt.grid()
+
+plt.show()
